d558917ea58e2e2bf46cc3b9612a7465007b2e52
Jason Long <jason.long77@hotmail.com>
2020-09-23 18:04:23 +0000

    Exported the setupInput function

---
+++ input.js
@@ -0,0 +1,19 @@
+const handleUserInput = function () {
+  if (key === "\u0003") {
+    process.exit();
+  }
+};
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", (key) => {
+    handleUserInput(key);
+  })
+  return stdin;
+};
+
+module.exports = {setupInput};
+
