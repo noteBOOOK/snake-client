aaf11af2467a2e8180129671e9ae77f316fe215c
Jason Long <jason.long77@hotmail.com>
2020-09-23 19:22:53 +0000

    Created constants module

---
+++ constants.js
@@ -0,0 +1,27 @@
+const IP = "135.23.222.131"
+const PORT = 50542
+
+
+const chatLog = {
+  g: "Say: I'm the best around",
+  h: "Say: nothing's gonna ever",
+  j: "Say: keep me down",
+  b: "Say: What is this",
+  n: "Say: a server for snakes?"
+};
+
+const movement = {
+  w: "Move: up",
+  a: "Move: left",
+  s: "Move: down",
+  d: "Move: right"
+};
+
+module.exports = {
+  IP,
+  PORT,
+  chatLog,
+  movement
+};
+
+
