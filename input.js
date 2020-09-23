f60415a76eaa12cab887ad8485bd24b033e67a57
Jason Long <jason.long77@hotmail.com>
2020-09-23 18:57:33 +0000

    Added chat functions

--- input.js
+++ input.js
@@ -23,6 +23,22 @@ const setupInput = function(conn) {
       connection.write("Move: right");
     }
   });
+  stdin.on("data", (key) => {
+    if (key === "g") {
+      connection.write("Say: I'm the best around");
+    } else if (key === "h") {
+      connection.write("Say: Nothing's gonna ever keep me down");
+    } else if (key === "j") {
+      connection.write("Say: gonna keep me down");
+    }
+  });
+  stdin.on("data", (key) => {
+    if (key === "b") {
+      connection.write("Say: What is this")
+    } else if (key === "n") {
+      connection.write("Say: a server for snakes?");
+    }
+  })
   return stdin;
 };
 
