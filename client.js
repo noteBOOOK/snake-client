4c7daf9c4e54cccdfce0e7133fac53e92d029401
Jason Long <jason.long77@hotmail.com>
2020-09-23 17:33:19 +0000

    Created Client and Play files

---
+++ client.js
@@ -0,0 +1,16 @@
+const net = require("net");
+
+const connect = function() {
+  const conn = net.createConnection({
+    host: "localhost",
+    port: 50541
+  });
+  // Interpret incoming data as text
+  conn.setEncoding("utf8");
+  conn.on("data", (data) => {
+    console.log(data);
+  })
+  return conn;
+};
+
+module.exports = connect;
