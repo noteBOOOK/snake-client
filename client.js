f4e9062981b2a0a9514240b5f0466f528ef84a2f
Jason Long <jason.long77@hotmail.com>
2020-09-23 19:32:26 +0000

    Changed to server IP and PORT

--- client.js
+++ client.js
@@ -3,10 +3,9 @@ const {IP, PORT} = require("./constants");
 
 const connect = function() {
   const conn = net.createConnection({
-    host: "localhost",
-    port: 50541
+    host: IP,
+    port: PORT
   });
-  // Interpret incoming data as text
   conn.setEncoding("utf8");
   
   conn.on("data", (data) => {
