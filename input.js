dbc62b5c502413d721701f5d0db958b3b17e6cf5
Jason Long <jason.long77@hotmail.com>
2020-09-23 18:48:07 +0000

    Created WASD movement

--- input.js
+++ input.js
@@ -1,17 +1,28 @@
-const handleUserInput = function () {
-  if (key === "\u0003") {
-    process.exit();
-  }
-};
+let connection; 
 
-const setupInput = function() {
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
+  
   stdin.on("data", (key) => {
-    handleUserInput(key);
-  })
+    if (key === "\u0003") {
+      process.exit();
+    };
+  });
+  stdin.on("data", (key) => {
+    if (key === "w") {
+      connection.write("Move: up");
+    } else if (key === "s") {
+      connection.write("Move: down");
+    } else if (key === "a") {
+      connection.write("Move: left");
+    } else if (key === "d") {
+      connection.write("Move: right");
+    }
+  });
   return stdin;
 };
 
